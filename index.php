cf566145d4c5afbabb1ba57e30ab77e8a85d10b8
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2025-05-21 00:04:31 +0100

    Add homepage

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
 <?php
+
+
+declare(strict_types=1);
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Phlo\Core\{Router, Rule, RuleType};
+
+try {
+	Router::new()
+		->addRule(Rule::new("public")->setRuleType(RuleType::STATIC)->setTarget(__DIR__ . "/public"))
+		->addRule(Rule::new("")->setRuleType(RuleType::STATIC)->setTarget(__DIR__ . "/pages"))
+		->serve();
+} catch (Exception | Error $e) {
+	echo "Internal Server Error: " . $e->getMessage() . PHP_EOL;
+}
---
+++ pages/index.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+use Burrow\Client;
+
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+// TODO: refactor into a `Lobster` class
+// TODO: add item page
+$client = new Client([
+	"baseUrl" => "https://lobste.rs/active/page",
+	"object" => true,
+]);
+$page = $_GET['page'] ?? 1;
+$data = $client->get("/{$page}.json")->data;
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="/public/style.css">
+	<title>Burrow</title>
+</head>
+
+<body class="container">
+	<h1>Burrow</h1>
+	<?php foreach ($data as $i => $item): ?>
+		<div class="item">
+			<p class="number">
+				<?= ($i + 1) + (($page - 1) * count($data)) ?>
+			</p>
+			<div class="w-full flex flex-col gap-1">
+				<a href="/<?= $item->shortId ?>"><?= $item->title ?></a>
+
+				<div class="flex align-center justify-between gap-1 flex-wrap">
+					<div class="flex align-center gap-1 flex-wrap">
+						<p class="text-muted small"><?= $item->score ?> points</p>
+						<p class="text-muted small">&bull;</p>
+						<p class="text-muted small"><?= $item->commentCount ?> comments</p>
+						<p class="text-muted small">&bull;</p>
+						<p class="text-muted small"><?= $item->submitterUser ?></p>
+						<p class="text-muted small">&bull;</p>
+						<p class="text-muted small"><?= DateTime::createFromFormat('Y-m-d\TH:i:s.uP', $item->createdAt)->format('F j, Y G:i') ?></p>
+					</div>
+
+
+					<div class="flex align-center gap-2 flex-wrap">
+						<?php foreach ($item->tags as $tag): ?>
+							<a href="/tag/<?= $tag ?>" class="tag"><?= $tag ?></a>
+						<?php endforeach; ?>
+					</div>
+				</div>
+			</div>
+		</div>
+	<?php endforeach; ?>
+
+	<div class="pagination">
+		<?php if ($page > 1): ?>
+			<a href="/?page=<?= $page - 1 ?>">&laquo; Previous</a>
+		<?php endif; ?>
+		<div class="page-info">
+			<span><?= $page ?></span>
+		</div>
+		<a href="/?page=<?= $page + 1 ?>">Next &raquo;</a>
+	</div>
+</body>
+
+</html>
